6712ada1a9381697a9dadec2d01ae0c04dc6fb5a
Hirad Nikoo <hirad.nicol@gmail.com>
2018-10-13 10:34:01 +0330

    fix

--- mhub.api/apis/google.py
+++ mhub.api/apis/google.py
@@ -1,4 +1,4 @@
-from googleapiclient.discovery import build
+# from googleapiclient.discovery import build
 from config import configuration
 import pprint
 
@@ -7,6 +7,7 @@ class Google(object):
         self.cx_youtube_search = configuration("google", "cx", "youtube-search")
 
     def search_youtube(self, q):
-        service = build("customsearch", "v1", developerKey=configuration("google", "api_key"))
-        res = service.cse().list(q=q, cx=self.cx_youtube_search).execute()
-        pprint.pprint(res)
+        print('hi')
+        #service = build("customsearch", "v1", developerKey=configuration("google", "api_key"))
+        #res = service.cse().list(q=q, cx=self.cx_youtube_search).execute()
+        #pprint.pprint(service)
--- mhub.api/index.py
+++ mhub.api/index.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 import pprint
 @app.route("/")
 def index():
-    pprint.pprint(google.Google().search_youtube('michael jackson'))
+    #pprint.pprint(google.Google().search_youtube('michael jackson'))
     return "This is mhub api home!"
+
+
+@app.route("/search/<q>")
+def search(q):
+    return "Your search word is " + q
